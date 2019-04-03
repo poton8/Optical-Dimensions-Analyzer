7d3f0a83259c4f8a6613d9ed2d0eb381d9c33fe0
poton8 <pagilestennis@gmail.com>
2019-04-02 19:30:41 -0500

    updated calibration.py file save, and got test.py to make disparity map

--- Calibration.py
+++ Calibration.py
@@ -2,14 +2,14 @@ import numpy as np
 import cv2
 import glob
 import os
-import time as t
+
 
 # termination criteria
 criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 
 # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
-objp = np.zeros((6*7,3), np.float32)
-objp[:,:2] = np.mgrid[0:7,0:6].T.reshape(-1,2)
+objp = np.zeros((6*8,3), np.float32)
+objp[:,:2] = np.mgrid[0:8,0:6].T.reshape(-1,2)
 
 # Arrays to store object points and image points from all the images.
 objpoints = [] # 3d point in real world space
@@ -23,7 +23,7 @@ for fname in images:
     gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 
     # Find the chess board corners
-    ret, corners = cv2.findChessboardCorners(gray, (7,6),None)
+    ret, corners = cv2.findChessboardCorners(gray, (8,6),None)
 
     # If found, add object points, image points (after refining them)
     if ret == True:
@@ -33,7 +33,7 @@ for fname in images:
         imgpoints.append(corners2)
 
         # Draw and display the corners
-        img = cv2.drawChessboardCorners(img, (7,6), corners2,ret)
+        img = cv2.drawChessboardCorners(img, (8,6), corners2,ret)
         cv2.imshow('img',img)
         cv2.waitKey(500)
 
@@ -43,9 +43,7 @@ for fname in images:
 
 
 ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
-#dist = np.array([-0.13615181, 0.53005398, 0, 0, 0])
-
-print(dist)
+images = glob.glob('*.jpg')
 for fname in images:
     img = cv2.imread(fname)
     h, w = img.shape[:2]
@@ -57,3 +55,7 @@ for fname in images:
     name = fname.replace('.jpg','.png')
     cv2.imwrite('calibrated{}'.format(name),dst)
     cv2.destroyAllWindows()
+np.save('mtx', mtx)
+np.save('dist', dist)
+np.save('newcameramtx', newcameramtx)
+np.save('roi', roi)
--- Test.py
+++ Test.py
@@ -1,29 +1,65 @@
 import numpy as np
 import cv2
 from matplotlib import pyplot as plt
+from sklearn.preprocessing import normalize
 
-
-CAMERA_WIDTH = 1920
-CAMERA_HEIGHT = 1080
+print('loading images...')
 capL = cv2.VideoCapture(1)
 capR = cv2.VideoCapture(2)
-capL.set(cv2.CAP_PROP_FRAME_WIDTH, CAMERA_WIDTH)
-capL.set(cv2.CAP_PROP_FRAME_HEIGHT, CAMERA_HEIGHT)
-capR.set(cv2.CAP_PROP_FRAME_WIDTH, CAMERA_WIDTH)
-capR.set(cv2.CAP_PROP_FRAME_HEIGHT, CAMERA_HEIGHT)
-stereoMatcher = cv2.StereoBM_create()
-
-
-
 ret, imgL = capL.read()
 ret2, imgR = capR.read()
-grayLeft = cv2.cvtColor(imgL, cv2.COLOR_BGR2GRAY)
-grayRight = cv2.cvtColor(imgR, cv2.COLOR_BGR2GRAY)
-depth = stereoMatcher.compute(grayLeft, grayRight)
-plt.imshow(depth, cmap='gray')
-'''
-plt.title('Left'), plt.xticks([]), plt.yticks([])
-plt.subplot(122), plt.imshow(imgR, cmap='gray')
-plt.title('Right'), plt.xticks([]), plt.yticks([])'''
+mtx = np.load('mtx.npy')
+dist = np.load('dist.npy')
+newcameramtx = np.load('newcameramtx.npy')
+roi = np.load('roi.npy')
+x,y,w,h = roi
+dstL = cv2.undistort(imgL, mtx, dist, None, newcameramtx)
+dstR = cv2.undistort(imgR, mtx, dist, None, newcameramtx)
+dstL = dstL[y:y + h, x:x + w]
+dstR = dstR[y:y + h, x:x + w]
 
-plt.show()
+# SGBM Parameters -----------------
+window_size = 3  # wsize default 3; 5; 7 for SGBM reduced size image; 15 for SGBM full size image (1300px and above); 5 Works nicely
+
+left_matcher = cv2.StereoSGBM_create(
+    minDisparity=0,
+    numDisparities=160,  # max_disp has to be dividable by 16 f. E. HH 192, 256
+    blockSize=5,
+    P1=8 * 3 * window_size ** 2,
+    # wsize default 3; 5; 7 for SGBM reduced size image; 15 for SGBM full size image (1300px and above); 5 Works nicely
+    P2=32 * 3 * window_size ** 2,
+    disp12MaxDiff=1,
+    uniquenessRatio=15,
+    speckleWindowSize=0,
+    speckleRange=2,
+    preFilterCap=63,
+    mode=cv2.STEREO_SGBM_MODE_SGBM_3WAY
+)
+
+right_matcher = cv2.ximgproc.createRightMatcher(left_matcher)
+
+
+# FILTER Parameters
+lmbda = 80000
+sigma = 1.2
+visual_multiplier = 1.0
+
+wls_filter = cv2.ximgproc.createDisparityWLSFilter(matcher_left=left_matcher)
+wls_filter.setLambda(lmbda)
+wls_filter.setSigmaColor(sigma)
+
+
+print('computing disparity...')
+displ = left_matcher.compute(imgL, imgR)  # .astype(np.float32)/16
+dispr = right_matcher.compute(imgR, imgL)  # .astype(np.float32)/16
+displ = np.int16(displ)
+dispr = np.int16(dispr)
+filteredImg = wls_filter.filter(displ, imgL, None, dispr)  # important to put "imgL" here!!!
+
+
+filteredImg = cv2.normalize(src=filteredImg, dst=filteredImg, beta=0, alpha=255, norm_type=cv2.NORM_MINMAX);
+filteredImg = np.uint8(filteredImg)
+
+cv2.imshow('Disparity Map', filteredImg)
+cv2.waitKey()
+cv2.destroyAllWindows()
